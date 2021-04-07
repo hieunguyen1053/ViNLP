59c5f5be733eff6653389acf0c8d5327c1842e09
Hieu Nguyen <hieunguyen1053@outlook.com>
2021-04-07 22:44:38 +0700

    remove accent function

--- ViNLP/utils/accent.py
+++
@@ -1,8 +0,0 @@
-data = open("/Users/hieunguyen/Desktop/ViNLP/ViNLP/data/vi.accent").read().split("\n")
-ACCENT_MAP = {}
-REVERSE_ACCENT_MAP = {}
-
-for line in data:
-    token, label = line.split()
-    ACCENT_MAP[token] = label
-    REVERSE_ACCENT_MAP[label] = token
