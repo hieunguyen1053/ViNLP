4d4f68b9e52ffa07bd8e018c7fdc30387af325da
Hieu Nguyen <hieunguyen1053@outlook.com>
2021-06-17 22:49:02 +0700

    update ViNLP

--- ViNLP/__init__.py
+++ ViNLP/__init__.py
@@ -1,4 +1,6 @@
 from .pipelines.word_tokenize import word_tokenize
 from .pipelines.pos_tag import pos_tag
+from .pipelines.chunking import chunk
+from .pipelines.ner import ner
 
 __author__ = "Nguyễn Trọng Hiếu"
