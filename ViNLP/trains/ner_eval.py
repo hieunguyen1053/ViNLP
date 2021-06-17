0c8a5a17306810ef3d523bbcb4763309ca56297e
Hieu Nguyen <hieunguyen1053@outlook.com>
2021-06-17 22:48:51 +0700

    add ner

---
+++ ViNLP/trains/ner_eval.py
@@ -0,0 +1,53 @@
+from ..datasets.ner_dataset import NERDataset
+from ..features.ner_feature import NERFeature
+from ..models.ner_crf import NER_CRF
+
+
+def _get_tags(sents):
+    tags = []
+    for sent_idx, iob_tags in enumerate(sents):
+        curr_tag = {'type': None, 'start_idx': None,
+                    'end_idx': None, 'sent_idx': None}
+        for i, tag in enumerate(iob_tags):
+            if tag == 'O' and curr_tag['type']:
+                tags.append(tuple(curr_tag.values()))
+                curr_tag = {'type': None, 'start_idx': None,
+                            'end_idx': None, 'sent_idx': None}
+            elif tag.startswith('B'):
+                curr_tag['type'] = tag[2:]
+                curr_tag['start_idx'] = i
+                curr_tag['end_idx'] = i
+                curr_tag['sent_idx'] = sent_idx
+            elif tag.startswith('I'):
+                curr_tag['end_idx'] = i
+        if curr_tag['type']:
+            tags.append(tuple(curr_tag.values()))
+    tags = set(tags)
+    return tags
+
+
+def f_measure(y_true, y_pred):
+    tags_true = _get_tags(y_true)
+    tags_pred = _get_tags(y_pred)
+
+    ne_ref = len(tags_true)
+    ne_true = len(set(tags_true).intersection(tags_pred))
+    ne_sys = len(tags_pred)
+    if ne_ref == 0 or ne_true == 0 or ne_sys == 0:
+        return 0
+    p = ne_true / ne_sys
+    r = ne_true / ne_ref
+    f1 = (2 * p * r) / (p + r)
+
+    return f1
+
+
+test_set = NERDataset('ViNLP/data/vlsp2016/test.txt')
+
+postag_features = NERFeature()
+X_test, y_test = postag_features.transform(test_set.data)
+
+
+model = NER_CRF.load("ViNLP/pipelines/bin/ner.crfsuite")
+y_pred = model.predict(X_test)
+print(f_measure(y_test, y_pred))
---
+++ ViNLP/trains/ner_train.py
@@ -0,0 +1,21 @@
+from ..datasets.ner_dataset import NERDataset
+from ..features.ner_feature import NERFeature
+from ..models.ner_crf import NER_CRF
+
+train_set = NERDataset('ViNLP/data/vlsp2016/train.txt')
+dev_set = NERDataset('ViNLP/data/vlsp2016/dev.txt')
+
+postag_features = NERFeature()
+X_train, y_train = postag_features.transform(train_set.data)
+X_dev, y_dev = postag_features.transform(train_set.data)
+
+model = NER_CRF(
+    c1=1.0,
+    c2=1e-3,
+    max_iterations=100,
+    all_possible_transitions=True,
+    verbose=True,
+)
+
+model.fit(X_train, y_train, X_dev, y_dev)
+model.save("ViNLP/pipelines/bin/ner.crfsuite")
